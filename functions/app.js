fbd24fb458d422310946a121402281aa9f2e961d
unknown <kevin.feo@educar.com.co>
2021-06-09 10:50:59 -0500

    validation form

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const userRouter = require("./routes/user.router");
-
-
+const helmet = require('helmet');
+const bodyparser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 
-
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(helmet());
+app.use(cors());
 app.use( userRouter );
 
 
